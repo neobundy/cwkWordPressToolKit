bcf346c2697d0da440d327435ac81069b668d8da
Wankyu Choi <wankyuchoi@MacPro.local>
2014-09-18 18:19:45 +0900

    Adding: Easy Bootstrap Shortcode Pro features

--- cwkWordPressToolKit.py
+++ cwkWordPressToolKit.py
@@ -12,7 +12,7 @@ class CwkWordPressParser(sublime_plugin.TextCommand,):
 	def __init__(self, *args, **kwargs):
 		sublime_plugin.TextCommand.__init__(self, *args, **kwargs)
 
-	def run(self, edit, opening_tag="", closing_tag="", sub_opening_tag="", sub_closing_tag="", attr="", sub_attr="", use_header=False, use_sub_header=False, sub_header_delimiter="|"):
+	def run(self, edit, opening_tag="", closing_tag="", sub_opening_tag="", sub_closing_tag="", attr="", sub_attr="", use_header=False, use_sub_header=False, sub_header_delimiter="|", newline=True):
 
 		self.opening_tag = opening_tag
 		self.closing_tag = closing_tag
@@ -23,6 +23,7 @@ class CwkWordPressParser(sublime_plugin.TextCommand,):
 		self.use_header = use_header
 		self.use_sub_header = use_sub_header
 		self.sub_header_delimiter = '\\' + sub_header_delimiter
+		self.newline = newline
 
 
 		if self.opening_tag:
@@ -36,7 +37,7 @@ class CwkWordPressParser(sublime_plugin.TextCommand,):
 
 					if self.attr:
 						tag_delimiter = self.opening_tag[-1]
-						if self.use_header:
+						if self.use_header == 'True':
 							self.opening_tag = opening_tag[:-1] + " " + self.attr + "=" + '"' + currentSelection.splitlines()[0]  + '"' + tag_delimiter
 							currentSelection = currentSelection.splitlines()[1:]
 						else:
@@ -50,7 +51,7 @@ class CwkWordPressParser(sublime_plugin.TextCommand,):
 							self.sub_opening_tag = sub_opening_tag[:-1] + " " +  self.sub_attr + tag_delimiter						
 						lines = []
 						for line in currentSelection:
-							if self.use_sub_header:
+							if self.use_sub_header == 'True':
 								sub_lines = re.split(self.sub_header_delimiter, line)
 								if sub_lines:
 									sub_header = sub_lines[0].strip()
@@ -63,10 +64,16 @@ class CwkWordPressParser(sublime_plugin.TextCommand,):
 
 
 							lines.append( self.sub_opening_tag + line + self.sub_closing_tag )
-						currentSelection =  self.opening_tag + "\n" + "\n".join( lines )  + "\n" +  self.closing_tag
+						if self.newline != 'False':
+							currentSelection =  self.opening_tag + "\n" + "\n".join( lines )  + "\n" +  self.closing_tag
+						else:
+							currentSelection =  self.opening_tag + "".join( lines )  +  self.closing_tag
+
 					else: 
-						currentSelection = self.opening_tag + "\n" +  "\n".join( currentSelection ) + "\n" +  self.closing_tag
+						if self.newline != 'False':
+							currentSelection = self.opening_tag + "\n" +  "\n".join( currentSelection ) + "\n" +  self.closing_tag
+						else:
+							currentSelection = self.opening_tag + "".join( currentSelection )  +  self.closing_tag
 
 					view.replace(edit, region, currentSelection)
-
 
